63ee7ae977debe57dff175bf703a99cf6d4a7c3b
augustocsmiranda <augustocsmiranda@gmail.com>
2024-11-08 14:13:58 -0300

    CRUD Scaffolding

---
+++ AppVendas/Data/AppVendasContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppVendas.Models
+{
+    public class AppVendasContext : DbContext
+    {
+        public AppVendasContext (DbContextOptions<AppVendasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AppVendas.Models.Department> Department { get; set; }
+    }
+}
