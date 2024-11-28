28387990001e5ba84497aa4a868f4b77183443d0
augustocsmiranda <augustocsmiranda@gmail.com>
2024-11-28 11:10:04 -0300

    Seller details and eager loading

--- AppVendas/Controllers/SellersController.cs
+++ AppVendas/Controllers/SellersController.cs
@@ -51,5 +51,20 @@ namespace AppVendas.Controllers
             _sellerService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(int? id) {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
--- AppVendas/Services/SellerService.cs
+++ AppVendas/Services/SellerService.cs
@@ -2,6 +2,8 @@
 //using AspNetCore;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace AppVendas.Services
 {
@@ -27,7 +29,7 @@ namespace AppVendas.Services
         }
 
         public Seller FindById(int id) { 
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int Id)
